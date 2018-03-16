d88afa005ee6c853207716f268a401d2bf7bc7a2
Michel Sciortino <michel.sciortino@outlook.com>
2018-03-16 23:56:38 +0100

    Main View designed

--- COM Ports Communication/MainWindow.xaml.cs
+++ COM Ports Communication/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO.Ports;
 namespace COM_Ports_Communication
 {
     /// <summary>
@@ -23,6 +23,47 @@ namespace COM_Ports_Communication
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        public void getAvailablePortNames()
+        {
+            String[] ports = SerialPort.GetPortNames();
+            Ports.Items.Clear();
+            foreach(string port in ports)
+            {
+                ComboBoxItem cbi = new ComboBoxItem();
+                cbi.Content = port;
+                Ports.Items.Add(cbi);
+            }
+        }
+
+        private void PortsDropDownOpened(object sender, EventArgs e)
+        {
+            getAvailablePortNames();
+        }
+
+        private void ConnectionSettingSelected(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty((string)Ports.SelectedValue?.ToString()) && !string.IsNullOrEmpty((string)RateBounds.SelectedValue?.ToString()))
+            {
+                OpenPortButton.IsEnabled = true;
+            }
+            else
+            {
+                OpenPortButton.IsEnabled = false;
+                ClosePortButton.IsEnabled = false;
+            }
+        }
+
+        private void OpenPortButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClosePortButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
