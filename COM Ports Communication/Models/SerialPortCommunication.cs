8f027076d7c7e21878a4b513bb3108ec2066c39f
Michel Sciortino <michel.sciortino@outlook.com>
2018-03-20 14:58:29 +0100

    quick cleaning

--- COM Ports Communication/Models/SerialPortCommunication.cs
+++ COM Ports Communication/Models/SerialPortCommunication.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Threading;
 
 namespace COM_Ports_Communication.Models
 {
--- COM Ports Communication/ViewModels/MainWindowViewModel.cs
+++ COM Ports Communication/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows.Controls;
 using GalaSoft.MvvmLight;
 namespace COM_Ports_Communication.ViewModels
